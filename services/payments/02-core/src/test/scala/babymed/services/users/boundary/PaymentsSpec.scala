4f72ebc78f131c15e0f49f6410b92a8c150f21b7
Surojiddin <isurojiddin@gmail.com>
2022-11-01 10:33:20 +0500

    get payments total & tests

--- services/payments/02-core/src/test/scala/babymed/services/users/boundary/PaymentsSpec.scala
+++ services/payments/02-core/src/test/scala/babymed/services/users/boundary/PaymentsSpec.scala
@@ -6,6 +6,7 @@ import babymed.services.payments.repositories.PaymentsRepository
 import babymed.services.payments.generators.PaymentGenerator
 import babymed.test.TestSuite
 import cats.effect.kernel.Sync
+import org.scalacheck.Gen
 
 object PaymentsSpec extends TestSuite with PaymentGenerator {
   val paymentRepo: PaymentsRepository[F] = new PaymentsRepository[F] {
@@ -13,6 +14,8 @@ object PaymentsSpec extends TestSuite with PaymentGenerator {
       Sync[F].delay(paymentGen.get)
     override def get(searchFilters: SearchFilters): F[List[PaymentWithCustomer]] =
       Sync[F].delay(List(paymentWithCustomerGen.get))
+    override def getPaymentsTotal(filters: SearchFilters): PaymentsSpec.F[Long] =
+      Sync[F].delay(Gen.long.get)
   }
 
   val payments: Payments[F] = new Payments[F](paymentRepo)
@@ -31,6 +34,17 @@ object PaymentsSpec extends TestSuite with PaymentGenerator {
   loggedTest("Get Payments by Filters") { implicit logger =>
     payments
       .get(searchFiltersGen.get)
+      .as(success)
+      .handleErrorWith { error =>
+        logger
+          .error("Error occurred!", cause = error)
+          .as(failure("Test failed!"))
+      }
+  }
+
+  loggedTest("Get Payments Total") { logger =>
+    payments
+      .getPaymentsTotal(SearchFilters.Empty)
       .as(success)
       .handleErrorWith { error =>
         logger
