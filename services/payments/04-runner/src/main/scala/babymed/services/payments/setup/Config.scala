7dfb73a168f8dc2494e3bc53be87639569d7bac2
prince <Prince777_98@mail.ru>
2022-11-03 14:06:25 +0500

    common sql migration

--- services/payments/04-runner/src/main/scala/babymed/services/payments/setup/Config.scala
+++ services/payments/04-runner/src/main/scala/babymed/services/payments/setup/Config.scala
@@ -3,9 +3,11 @@ package babymed.services.payments.setup
 import eu.timepit.refined.types.string.NonEmptyString
 
 import babymed.domain.AppMode
+import babymed.support.database.MigrationsConfig
 import babymed.support.services.http4s.HttpServerConfig
 import babymed.support.services.rpc.GrpcServerConfig
 import babymed.support.skunk.DataBaseConfig
+import babymed.syntax.refined.commonSyntaxAutoUnwrapV
 
 case class Config(
     appMode: AppMode,
@@ -13,4 +15,14 @@ case class Config(
     rpcServer: GrpcServerConfig,
     httpServer: HttpServerConfig,
     database: DataBaseConfig,
+  ) {
+  lazy val migrations: MigrationsConfig = MigrationsConfig(
+    hostname = database.host,
+    port = database.port,
+    database = database.database,
+    username = database.user,
+    password = database.password.value,
+    schema = "public",
+    location = "db/migration",
   )
+}
