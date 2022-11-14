c3e337a389418c47bb69540e4343f8fb7e955579
Surojiddin <isurojiddin@gmail.com>
2022-11-14 17:50:51 +0500

    create & get services routers

---
+++ services/babymed-api/00-core/src/main/scala/babymed/services/babymed/api/routes/ServiceRouters.scala
@@ -0,0 +1,41 @@
+package babymed.services.babymed.api.routes
+
+import cats.effect.Async
+import cats.implicits._
+import org.http4s._
+import org.http4s.circe.JsonDecoder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+import org.typelevel.log4cats.Logger
+
+import babymed.domain.Role.Doctor
+import babymed.services.auth.impl.Security
+import babymed.services.users.domain.User
+import babymed.services.visits.domain.CreateService
+import babymed.services.visits.proto.Services
+import babymed.support.services.syntax.all._
+
+final case class ServiceRouters[F[_]: Async: JsonDecoder](
+    security: Security[F],
+    services: Services[F],
+  )(implicit
+    logger: Logger[F]
+  ) extends Http4sDsl[F] {
+  private[routes] val prefixPath = "/service"
+
+  private[this] val privateRoutes: AuthedRoutes[User, F] = AuthedRoutes.of {
+
+    case ar @ POST -> Root / "create" as user if user.role != Doctor =>
+      ar.req.decodeR[CreateService] { createService =>
+        services.create(createService) *> NoContent()
+      }
+
+    case GET -> Root / "report" as _ =>
+      services.get.flatMap(Ok(_))
+
+  }
+
+  lazy val routes: HttpRoutes[F] = Router(
+    prefixPath -> security.auth.usersMiddleware(security.userJwtAuth)(privateRoutes)
+  )
+}
