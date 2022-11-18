b7eb7d456f868c82319910915d5ef74b24aaf146
Navruz <safabayev@mail.ru>
2022-11-18 10:00:22 +0500

    *

--- migrations/src/main/resources/db/migration/0XX/V002__message_service_tables.sql
+++ migrations/src/main/resources/db/migration/0XX/V002__message_service_tables.sql
@@ -12,19 +12,20 @@ VALUES ('sent'),
 
 CREATE TABLE IF NOT EXISTS message_types
 (
-   name    VARCHAR NOT NULL PRIMARY KEY,
-   deleted BOOLEAN NOT NULL DEFAULT false
+    name    VARCHAR NOT NULL PRIMARY KEY,
+    deleted BOOLEAN NOT NULL DEFAULT false
 );
 
-INSERT INTO message_types VALUES ('registration');
+INSERT INTO message_types
+VALUES ('registration');
 
 CREATE TABLE IF NOT EXISTS messages
 (
     id              UUID PRIMARY KEY,
-    sent_date       TIMESTAMP       NOT NULL,
-    phone           VARCHAR         NOT NULL,
-    text            VARCHAR         NOT NULL,
-    message_type VARCHAR   NOT NULL
+    sent_date       TIMESTAMP NOT NULL,
+    phone           VARCHAR   NOT NULL,
+    text            VARCHAR   NOT NULL,
+    message_type    VARCHAR   NOT NULL
         CONSTRAINT fk_message_type REFERENCES message_types (name) ON UPDATE CASCADE ON DELETE NO ACTION,
     delivery_status VARCHAR   NOT NULL
         CONSTRAINT fk_delivery_status REFERENCES delivery_statuses (name) ON UPDATE CASCADE ON DELETE NO ACTION DEFAULT 'sent'
