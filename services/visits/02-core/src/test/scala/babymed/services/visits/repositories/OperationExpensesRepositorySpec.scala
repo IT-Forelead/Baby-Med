01406d1005a10ae0f36da42ac5204658f3f89c59
Surojiddin <isurojiddin@gmail.com>
2023-01-11 15:30:04 +0500

    new e2e tests added to operation service

--- services/visits/02-core/src/test/scala/babymed/services/visits/repositories/OperationExpensesRepositorySpec.scala
+++ services/visits/02-core/src/test/scala/babymed/services/visits/repositories/OperationExpensesRepositorySpec.scala
@@ -1,15 +1,16 @@
 package babymed.services.visits.repositories
 
-//import java.time.LocalDateTime
-//import cats.effect.Resource
-//import cats.implicits.catsSyntaxOptionId
-//import cats.implicits.toTraverseOps
-//import skunk.Session
-//import weaver.Expectations
+import java.time.LocalDateTime
 
 import cats.effect.IO
+import cats.effect.Resource
+import cats.implicits.catsSyntaxOptionId
+import cats.implicits.toTraverseOps
+import skunk.Session
+import weaver.Expectations
 
 import babymed.services.visits.domain.OperationExpenseFilters
+import babymed.services.visits.domain.OperationFilters
 import babymed.services.visits.generators.OperationExpenseGenerators
 import babymed.support.database.DBSuite
 
@@ -17,29 +18,29 @@ object OperationExpensesRepositorySpec extends DBSuite with OperationExpenseGene
   override def schemaName: String = "public"
   override def beforeAll(implicit session: Res): IO[Unit] = data.setup
 
-//  test("Get Operation Expenses") { implicit postgres =>
-//    val repo = OperationExpensesRepository.make[F]
-//    object Case1 extends TestCase[Res] {
-//      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
-//        repo
-//          .get(OperationExpenseFilters(endDate = LocalDateTime.now().minusMinutes(1).some))
-//          .map { operationExpense =>
-//            assert(operationExpense.isEmpty)
-//          }
-//    }
-//    object Case2 extends TestCase[Res] {
-//      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
-//        repo
-//          .get(OperationExpenseFilters(startDate = LocalDateTime.now().minusMinutes(1).some))
-//          .map { operationExpenses =>
-//            assert(operationExpenses.length == 3)
-//          }
-//    }
-//    List(
-//      Case1,
-//      Case2,
-//    ).traverse(_.check).map(_.reduce(_ and _))
-//  }
+  test("Get Operation Expenses") { implicit postgres =>
+    val repo = OperationExpensesRepository.make[F]
+    object Case1 extends TestCase[Res] {
+      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
+        repo
+          .get(OperationExpenseFilters(endDate = LocalDateTime.now().minusMinutes(1).some))
+          .map { operationExpense =>
+            assert(operationExpense.isEmpty)
+          }
+    }
+    object Case2 extends TestCase[Res] {
+      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
+        repo
+          .get(OperationExpenseFilters(startDate = LocalDateTime.now().minusMinutes(1).some))
+          .map { operationExpenses =>
+            assert(operationExpenses.length == 3)
+          }
+    }
+    List(
+      Case1,
+      Case2,
+    ).traverse(_.check).map(_.reduce(_ and _))
+  }
 
   test("Get Operation Expenses Total") { implicit postgres =>
     OperationExpensesRepository
@@ -50,6 +51,67 @@ object OperationExpensesRepositorySpec extends DBSuite with OperationExpenseGene
       }
       .handleError {
         fail("Test failed.")
+      }
+  }
+
+  test("Get Operations") { implicit postgres =>
+    val repo = OperationExpensesRepository.make[F]
+    object Case1 extends TestCase[Res] {
+      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
+        repo
+          .getOperations(OperationFilters(endDate = LocalDateTime.now().minusMinutes(1).some))
+          .map { operationExpense =>
+            assert(operationExpense.isEmpty)
+          }
+    }
+    object Case2 extends TestCase[Res] {
+      override def check(implicit dao: Resource[IO, Session[IO]]): IO[Expectations] =
+        repo
+          .getOperations(OperationFilters(startDate = LocalDateTime.now().minusMinutes(1).some))
+          .map { operationExpenses =>
+            assert(operationExpenses.length == 3)
+          }
+    }
+    List(
+      Case1,
+      Case2,
+    ).traverse(_.check).map(_.reduce(_ and _))
+  }
+
+  test("Get Operations Total") { implicit postgres =>
+    OperationExpensesRepository
+      .make[F]
+      .getOperationsTotal(OperationFilters.Empty)
+      .map { total =>
+        assert(total == 3)
+      }
+      .handleError {
+        fail("Test failed.")
+      }
+  }
+
+  test("Create Operation Service") { implicit postgres =>
+    OperationExpensesRepository
+      .make[F]
+      .createOperationService(data.service.id1)
+      .map { c =>
+        assert(c.serviceId == data.service.id1)
+      }
+      .handleError {
+        fail("Test failed.")
+      }
+  }
+
+  test("Get All Operation Services") { implicit postgres =>
+    OperationExpensesRepository
+      .make[F]
+      .getOperationServices
+      .map { operationServices =>
+        assert(operationServices.nonEmpty)
+      }
+      .handleError { error =>
+        println("ERROR::::::::::::::::::: " + error)
+        failure("Test failed.")
       }
   }
 }
